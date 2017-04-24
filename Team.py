3cc13e67196bd7a7bdf51952c46ef79405b3c517
jeffreyscheng <jeffch@wharton.upenn.edu>
2017-04-24 01:12:28 -0400

    fixed probability sums

--- Team.py
+++ Team.py
@@ -1,7 +1,7 @@
+import math
 import numpy as np
 import pandas as pd
 import random
-from scipy.linalg import eig
 from Dialgarithm import *
 
 
@@ -15,7 +15,7 @@ class Team:
 
     def is_valid(self):
         list_of_names = [m_set.pokemon.dex_name for m_set in self.party.keys()]
-        return np.unique(list_of_names).size == len(list_of_names)
+        return np.unique(list_of_names).size == 6
 
     def is_fainted(self):
         return self.party[self.current] == 0
@@ -34,14 +34,15 @@ class Team:
 
     def has_living_counter(self, opponent):
         return len([mon for mon, health in self.party.items() if
-                          mon in Dialgarithm.counters_dict[opponent] and health > 0]) > 0
+                    mon in Dialgarithm.counters_dict[opponent] and health > 0]) > 0
 
     def switch(self, opponent=None):
         if opponent is None:
             alive = [mon for mon, health in self.party.items() if health > 0]
             self.current = random.choice(alive)
             return
-        alive_counters = [mon for mon, health in self.party.items() if mon in Dialgarithm.counters_dict[opponent] and health > 0]
+        alive_counters = [mon for mon, health in self.party.items() if
+                          mon in Dialgarithm.counters_dict[opponent] and health > 0]
         if len(alive_counters) > 0:
             self.current = random.choice(alive_counters)
         else:
@@ -51,9 +52,13 @@ class Team:
     def __str__(self):
         return ', '.join([mon.name for mon in self.party])
 
-    def get_usage_sum(self, list_of_movesets):
+    @staticmethod
+    def get_usage_sum(list_of_movesets):
         list_of_pokemon = list(set([mon.pokemon.unique_name for mon in list_of_movesets]))
-        return sum([Dialgarithm.usage_dict[mon] for mon in list_of_pokemon])
+        ans = sum([Dialgarithm.usage_dict[mon] for mon in list_of_pokemon])
+        if ans > 1:
+            raise ValueError("usage greater than 1")
+        return ans
 
     def set_ssp(self):
 
@@ -61,55 +66,60 @@ class Team:
 
         arr = np.zeros((6, 6))
         transition_mat = pd.DataFrame(data=arr, index=team_names, columns=team_names)
-        for column in team_names:
-            column_moveset = Dialgarithm.moveset_dict[column]
-            self_loop = self.get_usage_sum(Dialgarithm.counters_dict[column_moveset])
-            for row in team_names:
-                row_moveset = Dialgarithm.moveset_dict[row]
+        for row in team_names:
+            row_moveset = Dialgarithm.moveset_dict[row]
+            self_loop = 1 - self.get_usage_sum(Dialgarithm.counters_dict[row_moveset])
+            for column in team_names:
+                column_moveset = Dialgarithm.moveset_dict[column]
                 if row == column:
                     transition_mat.loc[row, column] = self_loop
                 else:
                     counters_column_not_row = [mon for mon in Dialgarithm.counters_dict[column_moveset]
                                                if mon not in Dialgarithm.counters_dict[row_moveset]]
                     transition_mat.loc[row, column] = self.get_usage_sum(counters_column_not_row)
-            normalization_factor = (1 - self_loop) / (sum(transition_mat[column]) - self_loop)
-            for row in team_names:
+            normalization_factor = (1 - self_loop) / (sum(transition_mat.loc[row, :]) - self_loop)
+            print(self_loop + (sum(transition_mat.loc[row, :]) - self_loop) * normalization_factor)
+            for column in team_names:
                 if row != column:
-                    transition_mat.loc[row, column] /= normalization_factor
+                    transition_mat.loc[row, column] *= normalization_factor
+
         for row in team_names:
             print(sum(transition_mat.loc[row, :]))
+            if math.isnan(sum(transition_mat.loc[row, :])):
+                print([mon.name for mon in self.party.keys()])
+                raise ValueError
 
-        # transition matrix = M
-        # transition_mat = np.matrix([
-        #     [.9, .075, 0.025],
-        #     [0.15, 0.8, 0.05],
-        #     [0.25, 0.25, 0.5]])
-        # eigenvalues, eigenvectors = eig(transition_mat, right=False, left=True)
-        # stationary = np.array(eigenvectors[:, np.where(np.abs(eigenvalues - 1.) < 1e-8)[0][0]].flat)
-        # stationary = stationary / np.sum(stationary)
-        # print(stationary)
-
-
-
-
-
+                # transition matrix = M
+                # transition_mat = np.matrix([
+                #     [.9, .075, 0.025],
+                #     [0.15, 0.8, 0.05],
+                #     [0.25, 0.25, 0.5]])
+                # eigenvalues, eigenvectors = eig(transition_mat, right=False, left=True)
+                # stationary = np.array(eigenvectors[:, np.where(np.abs(eigenvalues - 1.) < 1e-8)[0][0]].flat)
+                # stationary = stationary / np.sum(stationary)
+                # print(stationary)
 
 
 
-        # for row in damage_graph.index:
-        #     num_counters = len(self.counters[row])
-        #     counter_sum = 0
-        #     for col in damage_graph.columns:
-        #         a, b = self.get_switch_costs(row, col)
-        #         damage_graph.loc[row, col] = b
-        #         probability_graph.loc[row, col] = a
-        #         counter_sum += a
-        #     for col in probability_graph.columns:
-        #         if row != col:
-        #             card = probability_graph.loc[row, col]
-        #             probability_graph.loc[row, col] = card / counter_sum * num_counters / num_sets
-        #         if row == col:
-        #             probability_graph.loc[row, col] = 1 - num_counters / num_sets
-        # steady_state = probability_graph
-        # for i in range(10):
-        #     steady_state = steady_state.dot(probability_graph)
+
+
+
+
+
+                # for row in damage_graph.index:
+                #     num_counters = len(self.counters[row])
+                #     counter_sum = 0
+                #     for col in damage_graph.columns:
+                #         a, b = self.get_switch_costs(row, col)
+                #         damage_graph.loc[row, col] = b
+                #         probability_graph.loc[row, col] = a
+                #         counter_sum += a
+                #     for col in probability_graph.columns:
+                #         if row != col:
+                #             card = probability_graph.loc[row, col]
+                #             probability_graph.loc[row, col] = card / counter_sum * num_counters / num_sets
+                #         if row == col:
+                #             probability_graph.loc[row, col] = 1 - num_counters / num_sets
+                # steady_state = probability_graph
+                # for i in range(10):
+                #     steady_state = steady_state.dot(probability_graph)
--- Watchmaker.py
+++ Watchmaker.py
@@ -13,8 +13,13 @@ class Watchmaker:
         b = Battle()
         b.get_all_counters()
         # Metagame().precomputation()
-        t = Metagame().generate_team()
-        t.set_ssp()
+        for i in range(0,100):
+            t = Metagame().generate_team()
+            t.set_ssp()
+
+        #['Ditto_Imposter', 'Jolteon_All-Out Attacking', 'Rotom-Wash_Physically Defensive Pivot', 'Hydreigon_Choice Scarf', 'Keldeo_Choice Scarf', 'Talonflame_Revenge Killer']
+        # ['Venusaur_Sun Sweeper', 'Ditto_Imposter', 'Medicham_Life Orb', 'Jirachi_Specially Defensive', 'Rotom-Wash_Physically Defensive Pivot', 'Sceptile_Life Orb']
+        #    ['Shuckle_Holy Shuck!', 'Mew_Stallbreaker', 'Diancie_Defensive', 'Lopunny_Offensive Utility', 'Excadrill_Sand Rush Sweeper', 'Medicham_Life Orb']
 
         # next, test metagame generation with initial population + elos
         # each folder has xy_dex, xy_movesets, xy_damage_cache
